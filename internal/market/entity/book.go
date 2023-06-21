932b33e334ebe1acebda955273dc197e6b0e0bfc
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2023-06-21 10:12:30 -0300

    feat: add book entity and methods

---
+++ internal/market/entity/book.go
@@ -0,0 +1,93 @@
+package entity
+
+import (
+	"container/heap"
+	"sync"
+)
+
+type Book struct {
+	Orders        []*Order
+	Transactions  []*Transaction
+	OrdersChan    chan *Order
+	OrdersChanOut chan *Order
+	Wg            *sync.WaitGroup
+}
+
+func NewBook(orderChan chan *Order, orderChanOut chan *Order, wg *sync.WaitGroup) *Book {
+	return &Book{
+		Orders:        []*Order{},
+		Transactions:  []*Transaction{},
+		OrdersChan:    orderChan,
+		OrdersChanOut: orderChanOut,
+		Wg:            wg,
+	}
+}
+
+func (book *Book) Trade() {
+	buyOrders := NewOrderQueue()
+	sellOrders := NewOrderQueue()
+
+	heap.Init(buyOrders)
+	heap.Init(sellOrders)
+
+	for order := range book.OrdersChan {
+		if order.OrderType == "BUY" {
+			buyOrders.Push(order)
+			if sellOrders.Len() > 0 && sellOrders.Orders[0].Price <= order.Price {
+				sellOrder := sellOrders.Pop().(*Order)
+				if sellOrder.PendingShares > 0 {
+					transaction := NewTransaction(sellOrder, order, order.Shares, sellOrder.Price)
+					book.AddTransaction(transaction, book.Wg)
+					sellOrder.Transactions = append(sellOrder.Transactions, transaction)
+					order.Transactions = append(order.Transactions, transaction)
+					book.OrdersChanOut <- sellOrder
+					book.OrdersChanOut <- order
+					if sellOrder.PendingShares > 0 { // TODO: Looks redundant, maybe remove it
+						sellOrders.Push(sellOrder)
+					}
+				}
+			}
+		} else if order.OrderType == "SELL" {
+			sellOrders.Push(order)
+			if buyOrders.Len() > 0 && buyOrders.Orders[0].Price >= order.Price {
+				buyOrder := buyOrders.Pop().(*Order)
+				if buyOrder.PendingShares > 0 {
+					transaction := NewTransaction(order, buyOrder, order.Shares, buyOrder.Price)
+					book.AddTransaction(transaction, book.Wg)
+					buyOrder.Transactions = append(buyOrder.Transactions, transaction)
+					order.Transactions = append(order.Transactions, transaction)
+					book.OrdersChanOut <- buyOrder
+					book.OrdersChanOut <- order
+					if buyOrder.PendingShares > 0 { // TODO: Looks redundant, maybe remove it
+						buyOrders.Push(buyOrder)
+					}
+				}
+			}
+		}
+	}
+}
+
+func (book *Book) AddTransaction(transaction *Transaction, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	sellingShares := transaction.SellingOrder.PendingShares
+	buyingShares := transaction.BuyingOrder.PendingShares
+
+	minShares := sellingShares
+	if buyingShares < minShares {
+		minShares = buyingShares
+	}
+
+	transaction.SellingOrder.Investor.UpdateAssetPosition(transaction.SellingOrder.Asset.ID, -minShares)
+	transaction.AddSellOrderPendingShares(-minShares)
+
+	transaction.BuyingOrder.Investor.UpdateAssetPosition(transaction.BuyingOrder.Asset.ID, minShares)
+	transaction.AddBuyOrderPendingShares(-minShares)
+
+	transaction.CalculateTotal(transaction.BuyingOrder.Price)
+
+	transaction.CloseBuyOrder()
+	transaction.CloseSellOrder()
+
+	book.Transactions = append(book.Transactions, transaction)
+}
