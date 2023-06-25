b9dca9a9920ca9f83f5a4d848022b93b7e0ac5aa
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2023-06-25 12:10:49 -0300

    feat: add method to create a transaction from an order

--- backend/src/orders/orders.controller.ts
+++ backend/src/orders/orders.controller.ts
@@ -1,17 +1,26 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { InitTransactionDto } from './order.dto';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { InitTransactionDto, InputExecuteTransactionDto } from './order.dto';
 import { OrdersService } from './orders.service';
 
-@Controller('orders')
+@Controller('wallets/:wallet_id/orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
-  @Post()
-  async initTransaction(@Body() body: InitTransactionDto) {
-    return await this.ordersService.initTransaction(body);
+  @Get()
+  async all(@Param('wallet_id') wallet_id: string) {
+    return await this.ordersService.all({ wallet_id });
   }
 
-  async executeTransaction() {
-    await this.ordersService.executeTransaction({} as any);
+  @Post()
+  async initTransaction(
+    @Param('wallet_id') wallet_id: string,
+    @Body() body: Omit<InitTransactionDto, 'wallet_id'>,
+  ) {
+    return await this.ordersService.initTransaction({ wallet_id, ...body });
+  }
+
+  @Post('execute')
+  async executeTransaction(@Body() body: InputExecuteTransactionDto) {
+    await this.ordersService.executeTransaction(body);
   }
 }
--- backend/src/orders/orders.service.ts
+++ backend/src/orders/orders.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { OrderStatus } from '@prisma/client';
+import { OrderStatus, OrderType } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { InitTransactionDto, InputExecuteTransactionDto } from './order.dto';
 
@@ -13,6 +13,7 @@ export class OrdersService {
         wallet_id: filter.wallet_id,
       },
       include: {
+        Transactions: true,
         Asset: {
           select: {
             id: true,
@@ -44,9 +45,72 @@ export class OrdersService {
   }
 
   async executeTransaction(input: InputExecuteTransactionDto) {
-    // add transaction to order
-    // count the amount of assets in the wallet
-    // update the order status to OPEN or CLOSED
-    // update the asset price
+    // discard all the updates if one of the database changes fail
+    await this.prismaService.$transaction(async (prisma) => {
+      // get related order
+      const order = await prisma.order.findUniqueOrThrow({
+        where: { id: input.order_id },
+      });
+      // update order adding the transaction
+      await prisma.order.update({
+        where: {
+          id: input.order_id,
+        },
+        data: {
+          partial: order.partial - input.negotiated_shares,
+          status: input.status,
+          Transactions: {
+            create: {
+              broker_transaction_id: input.broker_transaction_id,
+              related_investor_id: input.related_investor_id,
+              shares: input.negotiated_shares,
+              price: input.price,
+            },
+          },
+        },
+      });
+      if (input.status === OrderStatus.CLOSED) {
+        await prisma.asset.update({
+          where: { id: order.asset_id },
+          data: {
+            price: input.price,
+          },
+        });
+        const walletAsset = await prisma.walletAsset.findUnique({
+          where: {
+            wallet_id_asset_id: {
+              asset_id: order.asset_id,
+              wallet_id: order.wallet_id,
+            },
+          },
+        });
+        if (walletAsset) {
+          // if it already has the asset in the wallet, update the amount
+          await prisma.walletAsset.update({
+            where: {
+              wallet_id_asset_id: {
+                asset_id: order.asset_id,
+                wallet_id: order.wallet_id,
+              },
+            },
+            data: {
+              shares:
+                order.type === OrderType.BUY
+                  ? walletAsset.shares + order.shares
+                  : walletAsset.shares - order.shares,
+            },
+          });
+        } else {
+          // only add to the wallet if it's a BUY order
+          await prisma.walletAsset.create({
+            data: {
+              asset_id: order.asset_id,
+              wallet_id: order.wallet_id,
+              shares: input.negotiated_shares,
+            },
+          });
+        }
+      }
+    });
   }
 }
