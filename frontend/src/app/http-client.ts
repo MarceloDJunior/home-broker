ade97d3cbeb4024b9982da096361ef754c70eba5
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2023-06-25 18:55:02 -0300

    feat: add httpClient with default configs

---
+++ frontend/src/app/http-client.ts
@@ -0,0 +1,30 @@
+import humps from 'humps';
+
+const baseUrl = 'http://localhost:8000';
+
+const defaultOptions: RequestInit = {
+  headers: {
+    'Content-type': 'application/json',
+  },
+  next: {
+    revalidate: 10,
+  },
+};
+
+export class HttpClient {
+  static async get(url: string): Promise<any> {
+    const response = await fetch(baseUrl + url, defaultOptions);
+    const json = await response.json();
+    return humps.camelizeKeys(json);
+  }
+
+  static async post(url: string, body: any): Promise<any> {
+    const response = await fetch(baseUrl + url, {
+      ...defaultOptions,
+      method: 'POST',
+      body,
+    });
+    const json = await response.json();
+    return humps.camelizeKeys(json);
+  }
+}
