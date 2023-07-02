a4b3105b8245bbea57a96f2f97a03fc85f3942cb
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2023-07-02 20:22:47 -0300

    feat: watch database changes and send server events

--- backend/src/wallets/wallet-assets/wallet-assets.service.ts
+++ backend/src/wallets/wallet-assets/wallet-assets.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Observable } from 'rxjs';
 import { PrismaService } from 'src/prisma/prisma.service';
+import {
+  WalletAsset,
+  WalletAsset as WalletAssetSchema,
+} from './wallet-asset.schema';
 
 @Injectable()
 export class WalletAssetsService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+    @InjectModel(WalletAssetSchema.name)
+    private walletAssetSchema: Model<WalletAssetSchema>,
+  ) {}
 
   all(filter: { wallet_id: string }) {
     return this.prismaService.walletAsset.findMany({
@@ -30,6 +41,40 @@ export class WalletAssetsService {
         shares: input.shares,
         version: 1,
       },
+    });
+  }
+
+  subscribeEvents(wallet_id: string): Observable<{
+    event: 'wallet-asset-updated';
+    data: WalletAsset;
+  }> {
+    return new Observable((observer) => {
+      this.walletAssetSchema
+        .watch(
+          [
+            {
+              $match: {
+                operationType: 'update',
+                'fullDocument.wallet_id': wallet_id,
+              },
+            },
+          ],
+          {
+            fullDocument: 'updateLookup',
+          },
+        )
+        .on('change', async (data) => {
+          console.log(data);
+          const walletAsset = await this.prismaService.walletAsset.findUnique({
+            where: {
+              id: String(data.fullDocument._id),
+            },
+          });
+          observer.next({
+            event: 'wallet-asset-updated',
+            data: walletAsset,
+          });
+        });
     });
   }
 }
