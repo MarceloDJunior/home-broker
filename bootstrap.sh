19a52958a7e31aa637a6b1276ab0baf7a6c170a2
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2023-07-23 12:05:07 -0300

    chore: update bootstrap script to handle errors

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,10 +2,16 @@
 echo "Starting all containers..."
 
 docker-compose -f backend/docker-compose.yaml up -d
+backend_result=$?
+
 docker-compose -f broker-service/docker-compose.yaml up -d
+broker_result=$?
 
-echo "Containers started successfully!"
-
-echo "To start the broker microservice, cd into the broker-service folder and run: go run cmd/trade/main.go"
-echo 'To start the backend service, run: docker exec backend-app-1 bash -c "npm run dev"'
-echo 'To start the frontend app, cd into the frontend folder and run: "npm run dev"'
+if [ $backend_result -eq 0 ] && [ $broker_result -eq 0 ]; then
+  echo "Containers started successfully!"
+  echo "To start the broker microservice, cd into the broker-service folder and run: go run cmd/trade/main.go"
+  echo 'To start the backend service, run: docker exec backend-app-1 bash -c "npm run dev"'
+  echo 'To start the frontend app, cd into the frontend folder and run: "npm run dev"'
+else
+  echo "Failed to start containers."
+fi
