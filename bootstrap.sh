c3b7dd5835896ae22885f6deba4ba52e4bc9290b
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2023-07-23 13:11:09 -0300

    refactor: remove unnecessary Dockerfile and update container names

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ broker_result=$?
 if [ $backend_result -eq 0 ] && [ $broker_result -eq 0 ]; then
   echo "Containers started successfully!"
   echo "To start the broker microservice, cd into the broker-service folder and run: go run cmd/trade/main.go"
-  echo 'To start the backend service, run: docker exec backend-app-1 bash -c "npm run dev"'
+  echo 'To start the backend service, run: docker exec home-broker-backend bash -c "npm run dev"'
   echo 'To start the frontend app, cd into the frontend folder and run: "npm run dev"'
 else
   echo "Failed to start containers."
